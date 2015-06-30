844456d4ab30414746927647b8c8165b899752a9
Joe Yang <275625769@qq.com>
2015-06-30 08:54:58 +0000

    添加测试工具子项目。

---
+++ dsf-test-tools/src/main/scala/com/joe/dsf/testtools/DSFClusterTest.scala
@@ -0,0 +1,12 @@
+package com.joe.dsf.testtools
+
+/**
+ * DSF集群测试工具，用于自动在本地启动多个DSFNode进程，检验是否能够成功组件集群。
+ * 在之后的测试中，也用作启动本地DSF集群，协助其他功能测试。
+ * Created by yang_yongzhou on 15-6-28.
+ */
+object DSFClusterTest {
+  def main(args: Array[String]) {
+
+  }
+}
